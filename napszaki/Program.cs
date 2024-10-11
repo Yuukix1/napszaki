1041608489b512173b2d1c03523ddc247baa6d98
BabakAdam <babaka@kkszki.hu>
2024-10-11 11:49:20 +0200

    :3

--- napszaki/Program.cs
+++ napszaki/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace napszaki
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+        internal class Program
         {
+            static void Main(string[] args)
+            {
+                DateTime now = DateTime.Now;
+
+                if (now.Hour < 12)
+                    Console.WriteLine("Jó reggelt!");
+                else if (now.Hour < 18)
+                    Console.WriteLine("Jó napot!");
+                else
+                    Console.WriteLine("Jó estét!");
+            }
         }
     }
-}
+
